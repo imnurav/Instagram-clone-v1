27fe167f3a6661015065a592dc0865c5d330d995
varun kumar <itsnurav@gmail.com>
2023-02-11 21:25:57 +0530

    update modal

--- pages/component/UploadModal.js
+++ pages/component/UploadModal.js
@@ -1,11 +1,70 @@
 import { useRecoilState } from "recoil";
 import { modalState } from "@/atom/modalAtom";
+import Modal from "react-modal";
+import { CameraIcon } from "@heroicons/react/outline";
+import { useRef, useState } from "react";
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState);
+  const filePickerRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const addImageToPost = (event) => {
+    const reader = new FileReader();
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+    }
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+    console.log(selectedFile);
+  };
   return (
     <div>
       <h1>Upload Modal</h1>
-      {open && <h1>The Modal is open</h1>}
+      {open && (
+        <Modal
+          isOpen={open}
+          onRequestClose={() => {
+            setOpen(false);
+            setSelectedFile(null);
+          }}
+          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            {selectedFile ? (
+              <img
+                src={selectedFile}
+                alt="image"
+                className="w-full max-h-[250px] object-cover cursor-pointer"
+                onClick={() => setSelectedFile(null)}
+              />
+            ) : (
+              <CameraIcon
+                onClick={() => filePickerRef.current.click()}
+                className="cursor-pointer h-14 bg-red-200 p-2 rounded-full border-2 text-red-500"
+              />
+            )}
+            <input
+              type="file"
+              hidden
+              ref={filePickerRef}
+              onChange={addImageToPost}
+            />
+            <input
+              className="m-4 border-none text-center w-full focus:ring-0"
+              type="text"
+              placeholder="Enter Your Caption"
+              required
+              maxLength={150}
+            />
+            <button
+              disabled
+              className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-125 disabled:text-gray-300"
+            >
+              Upload Post
+            </button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 }
