e45a8936790817b9db4966e9dda1cddccc04ae67
varun kumar <itsnurav@gmail.com>
2023-02-16 11:01:46 +0530

    added image in sigin section

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,20 +6,32 @@ export default function signin({ providers }) {
   return (
     <>
       <Header />
-      <div className="mt-40">
-        {Object.values(providers).map((provider) => (
-          <div key={provider.name} className="flex flex-col items-center">
-            <img
-            className="w-52 object-cover"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
-              alt="google-logo"
-            />
-            <p className="text-sm italic my-10 text-center">This website is created for learning purposes</p>
-            <button className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500" onClick={() => signIn(provider.id, { callbackUrl: "/" })}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
+      <div className="flex justify-center space-x-7 mt-20">
+        <img
+          className="hidden object-cover rotate-6 md:inline-flex md:w-48"
+          src="https://superviral.com.au/wp-content/uploads/2021/08/instagix-banner-graphic.png"
+          alt="instagram-image"
+        />
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name} className="flex flex-col items-center">
+              <img
+                className="w-52 object-cover"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+                alt="google-logo"
+              />
+              <p className="text-sm italic my-10 text-center">
+                This website is created for learning purposes
+              </p>
+              <button
+                className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500"
+                onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
